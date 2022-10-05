1153db630548806c0bd445653a2131b29636f27a
vinhnguyen1124 <nhvinh1@tma.com.vn>
2022-10-05 16:09:12 +0700

    test barcode scan

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>Test</div>
+  <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded" />
+</template>
+
+<script>
+import { StreamBarcodeReader } from "vue-barcode-reader";
+export default {
+  methods: {
+    onDecode(data) {
+      alert(data);
+      const sku = data.split("|")[0];
+      if (sku) {
+        alert(sku);
+      }
+    },
+    onLoaded() {
+      console.log("load");
+    },
+  },
+  name: "App",
+  components: {
+    StreamBarcodeReader,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+// import { createWebHashHistory } from "vue-router";
+// export default {
+//     history: createWebHashHistory(),
+//     routes: [{
+//         path: "/",
+//         name: "Home",
+//         component: () =>
+//             import ("@/views/Home.vue"),
+//     }, ],
+// };
